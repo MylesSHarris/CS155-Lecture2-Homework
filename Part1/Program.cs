0db4d1b16f5c4757b851290b8f8110c51070bfad
MylesSHarris <69069959+MylesSHarris@users.noreply.github.com>
2020-10-06 09:09:26 -0700

    commit

---
+++ Part1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = "myles";
+            string last = "harris";
+
+            string firstPiglatin = first.Substring(1, 1).ToUpper() + first.Substring(2) + first.Substring(0, 1) + "ay";
+            string lastPiglatin = last.Substring(1, 1).ToUpper() + last.Substring(2) + last.Substring(0, 1) + "ay";
+
+            Console.WriteLine(firstPiglatin + " " + lastPiglatin);
+        }
+    }
+}
---
+++ Part2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a temperature in degrees Fahrenheit: ");
+            
+            int fahrenheit = Int32.Parse(Console.ReadLine());
+            double celsius = 5.0 * (fahrenheit - 32) / 9;
+
+            Console.WriteLine(fahrenheit + " degrees Fahrenheit = " + Math.Round(celsius, 1) + " degrees Celsius");
+        }
+    }
+}
